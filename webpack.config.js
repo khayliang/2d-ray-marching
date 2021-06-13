724d19766944c35f859877dec595605cbcb84ae2
khayliang <khayliang@gmail.com>
2021-06-13 13:22:17 +0800

    change webpage name

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Ant Colony',
+      title: '2D Ray Marching',
     }),
   ],
   output: {
